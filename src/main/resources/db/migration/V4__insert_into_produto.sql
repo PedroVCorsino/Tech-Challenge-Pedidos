e89b53819376ae55bb3f1025bf108da602607b58
Pedro Vinicius <pedro.santos@metasix.com.br>
2023-08-20 17:15:28 -0300

    Corrigindo migrations para atender mudanças no codigo.

--- src/main/resources/db/migration/V4__insert_into_produto.sql
+++ src/main/resources/db/migration/V4__insert_into_produto.sql
@@ -1,28 +1,28 @@
 -- Lanches
-INSERT INTO Lanche (nome, descricao, preco) VALUES ('Hambúrguer', 'Delicioso hambúrguer artesanal', 10.99);
-INSERT INTO Lanche (nome, descricao, preco) VALUES ('Cheeseburguer', 'Hambúrguer com queijo derretido', 12.99);
-INSERT INTO Lanche (nome, descricao, preco) VALUES ('X-Bacon', 'Hambúrguer com bacon crocante', 14.99);
-INSERT INTO Lanche (nome, descricao, preco) VALUES ('Vegetariano', 'Lanche saudável com ingredientes naturais', 9.99);
-INSERT INTO Lanche (nome, descricao, preco) VALUES ('Frango', 'Lanche com suculento frango grelhado', 11.99);
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Hambúrguer', 'Delicioso hambúrguer artesanal', 10.99, 'LANCHE');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Cheeseburguer', 'Hambúrguer com queijo derretido', 12.99, 'LANCHE');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('X-Bacon', 'Hambúrguer com bacon crocante', 14.99, 'LANCHE');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Vegetariano', 'Produto saudável com ingredientes naturais', 9.99, 'LANCHE');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Frango', 'Produto com suculento frango grelhado', 11.99, 'LANCHE');
 
 -- -- Acompanhamentos
-INSERT INTO Acompanhamento (nome, descricao, preco) VALUES ('Batata Frita', 'Porção de batatas fritas crocantes', 5.99);
-INSERT INTO Acompanhamento (nome, descricao, preco) VALUES ('Onion Rings', 'Deliciosos anéis de cebola empanados', 6.99);
-INSERT INTO Acompanhamento (nome, descricao, preco) VALUES ('Nuggets', 'Pedacinhos de frango empanados', 7.99);
-INSERT INTO Acompanhamento (nome, descricao, preco) VALUES ('Salada Caesar', 'Salada fresca com molho Caesar', 8.99);
-INSERT INTO Acompanhamento (nome, descricao, preco) VALUES ('Mandioca Frita', 'Porção de mandioca frita crocante', 4.99);
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Batata Frita', 'Porção de batatas fritas crocantes', 5.99, 'ACOMPANHAMENTO');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Onion Rings', 'Deliciosos anéis de cebola empanados', 6.99, 'ACOMPANHAMENTO');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Nuggets', 'Pedacinhos de frango empanados', 7.99, 'ACOMPANHAMENTO');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Salada Caesar', 'Salada fresca com molho Caesar', 8.99, 'ACOMPANHAMENTO');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Mandioca Frita', 'Porção de mandioca frita crocante', 4.99, 'ACOMPANHAMENTO');
 
 -- -- Bebidas
-INSERT INTO Bebida (nome, descricao, preco) VALUES ('Refrigerante', 'Bebida gaseificada sabor cola', 4.99);
-INSERT INTO Bebida (nome, descricao, preco) VALUES ('Suco Natural', 'Suco de frutas frescas', 5.99);
-INSERT INTO Bebida (nome, descricao, preco) VALUES ('Água Mineral', 'Água sem gás', 2.99);
-INSERT INTO Bebida (nome, descricao, preco) VALUES ('Chá Gelado', 'Chá gelado sabor limão', 3.99);
-INSERT INTO Bebida (nome, descricao, preco) VALUES ('Café', 'Café fresco e quente', 2.99);
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Refrigerante', 'Produto gaseificada sabor cola', 4.99, 'BEBIDA');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Suco Natural', 'Suco de frutas frescas', 5.99, 'BEBIDA');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Água Mineral', 'Água sem gás', 2.99, 'BEBIDA');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Chá Gelado', 'Chá gelado sabor limão', 3.99, 'BEBIDA');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Café', 'Café fresco e quente', 2.99, 'BEBIDA');
 
 -- -- Sobremesas
-INSERT INTO Sobremesa (nome, descricao, preco) VALUES ('Bolo de Chocolate', 'Delicioso bolo de chocolate', 7.99);
-INSERT INTO Sobremesa (nome, descricao, preco) VALUES ('Pudim de Leite', 'Clássico pudim de leite condensado', 6.99);
-INSERT INTO Sobremesa (nome, descricao, preco) VALUES ('Sorvete de Morango', 'Sorvete cremoso sabor morango', 5.99);
-INSERT INTO Sobremesa (nome, descricao, preco) VALUES ('Mousse de Maracujá', 'Mousse refrescante de maracujá', 4.99);
-INSERT INTO Sobremesa (nome, descricao, preco) VALUES ('Torta de Limão', 'Torta doce com recheio de limão', 6.99);
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Bolo de Chocolate', 'Delicioso bolo de chocolate', 7.99, 'SOBREMESA');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Pudim de Leite', 'Clássico pudim de leite condensado', 6.99, 'SOBREMESA');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Sorvete de Morango', 'Sorvete cremoso sabor morango', 5.99, 'SOBREMESA');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Mousse de Maracujá', 'Mousse refrescante de maracujá', 4.99, 'SOBREMESA');
+INSERT INTO Produto (nome, descricao, preco, categoria) VALUES ('Torta de Limão', 'Torta doce com recheio de limão', 6.99, 'SOBREMESA');
 
